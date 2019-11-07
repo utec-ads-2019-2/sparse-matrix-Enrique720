90a5681baeed6bb420b59b07d8e15eee0fb8286a
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-11-06 21:20:41 -0500

    avance

--- matrix.h
+++ matrix.h
@@ -1,29 +1,82 @@
 #ifndef SPARSE_MATRIX_MATRIX_H
 #define SPARSE_MATRIX_MATRIX_H
-
+#include "node.h"
 #include <stdexcept>
 
 using namespace std;
 
+#include <vector>
 template <typename T>
 class Matrix {
 private:
-    Node<T> *root;
-    unsigned rows, columns;
+	vector <Header<T>*>* x;
+    vector <Header<T>*>* y;
+    unsigned const rows, columns;
+
+ /*   Node<T>* find(unsigned pX, unsigned pY){
+        int contX;
+        auto temp = root;
+        while(temp->down && temp->posY <= pY){
+            temp= temp->down;
+        }
+    }*/
 
 public:
-    Matrix(unsigned rows, unsigned columns);
 
-    void set(unsigned, unsigned, T);
+	Matrix(unsigned _rows, unsigned _columns):rows{_rows} , columns{_columns} {
+        x = new vector<Header<T>*>;
+        y = new vector<Header<T>*>;
+        int i;
+        for(i = 0; i < columns ; i++){
+            (*x).emplace_back(new Header<T>);
+        }
+        for(i = 0; i < rows ; i++){
+            (*y).emplace_back(new Header<T>);
+        }
+
+	}
+/*
+    bool set(unsigned pX , unsigned pY, T data){
+        auto nodoInsert = find(pX,pY);
+        if(!nodoInsert){
+            nodoInsert= new Node<T>(pX,pY,data);
+
+        }
+        return false;
+	}
+*/
+
+/*
     T operator()(unsigned, unsigned) const;
     Matrix<T> operator*(T scalar) const;
     Matrix<T> operator*(Matrix<T> other) const;
     Matrix<T> operator+(Matrix<T> other) const;
     Matrix<T> operator-(Matrix<T> other) const;
     Matrix<T> transpose() const;
-    void print() const;
+    */
 
-    ~Matrix();
+    void print() const{
+        int i;
+        int cont= 0;
+        for(auto it = y->cbegin() ; it != y->cend() ; it++){
+            auto temp = (*it)->next;
+                while (temp) {
+                    while (cont++ < temp->posX)
+                        cout << 0 << " ";
+                    cout << temp->data << " ";
+                    temp = temp->right;
+                }
+                while (cont++ < columns){
+                    cout << 0 <<  " " ;
+                }
+                cont = 0;
+                cout << endl;
+
+        }
+    }
+	
+    //~Matrix();
+
 };
 
 #endif //SPARSE_MATRIX_MATRIX_H
--- node.h
+++ node.h
@@ -7,12 +7,28 @@ class Matrix;
 template <typename T>
 class Node {
 protected:
-    Node<T> *next, *down;
-
+    Node<T> *right, *down;
+	T data;
+	int posX;
+	int posY;
 public:
-    explicit Node();
+	explicit Node() : data{ 0 },posX { 0 }, posY{ 0 }, right{ nullptr }, down{ nullptr } {}
+	explicit Node(int x, int y, T _data) {
+		data = _data;
+		posX = x;
+		posY = y;
+		right = nullptr;
+		down = nullptr;
+	}
+    friend class Matrix<T>;	
+};
 
-    friend class Matrix<T>;
+template <typename T>
+struct Header{
+    Node<T> *next;
+    Header():next{nullptr}{}
+
 };
 
 #endif //SPARSE_MATRIX_NODE_H
+	
